c34a197888ff04d1a46421a3e33d6f93f9eede65
yakserr <yassirnugroho48@gmail.com>
2025-08-03 15:02:16 +0700

    feat(copilot): add initial configuration for Copilot and nvim-cmp integration

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = true,
+        debounce = 75,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
+    "nvim-cmp",
+    optional = true,
+    opts = function(_, opts)
+      -- Disable tab mapping in cmp when copilot suggestion is visible
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local cmp = require("cmp")
+      local copilot_suggestion = require("copilot.suggestion")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if copilot_suggestion.is_visible() then
+            copilot_suggestion.accept()
+          elseif cmp.visible() then
+            cmp.select_next_item()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+}
