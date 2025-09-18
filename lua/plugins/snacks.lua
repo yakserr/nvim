e0ae90969015e6d5ee55215be3f4103f25c6cbc7
yakserr <yassirnugroho48@gmail.com>
2025-09-18 20:59:19 +0700

    update
    a

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {},
+    picker = {
+      sources = {
+        explorer = {
+          layout = { preset = "sidebar", preview = false, layout = { position = "right" } },
+        },
+      },
+    },
+  },
+}
