57753988b9691cf8d988bc56155f7577c4ed3ccf
yakserr <yassirnugroho48@gmail.com>
2025-11-10 11:20:09 +0700

    adding copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,55 @@
+-- Configure copilot.lua for VSCode-like behavior
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>"
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+        -- VSCode-like behavior: accept with Tab
+        -- Trigger happens automatically as you type
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+        typescript = true,
+        javascript = true,
+        vue = true,
+        typescriptreact = true,
+        javascriptreact = true,
+        svelte = true,
+        lua = true,
+      },
+    })
+  end,
+}
