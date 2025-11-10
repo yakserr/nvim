452f4abe1e4f3aaef4126f39204d193cc02edecd
yakserr <yassirnugroho48@gmail.com>
2025-11-10 10:55:34 +0700

    show hidden file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,5 +5,12 @@ return {
     window = {
       position = "right",
     },
+
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
   },
 }
