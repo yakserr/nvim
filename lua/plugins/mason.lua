b36e4140c41dec6c6f0f69df80ae1693e2ce002f
yakserr <yassirnugroho48@gmail.com>
2025-11-16 12:00:53 +0700

    add mason.nvim plugin configuration with ensured language servers

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,20 @@
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "vtsls",
+      "vue-language-server",
+      "eslint_d",
+      "eslint-lsp",
+      "tailwindcss-language-server",
+      "emmet-language-server",
+      "css-lsp",
+      "html-lsp",
+      "bash-language-server",
+      "shfmt",
+      "shellcheck",
+      "stylua",
+      "lua-language-server",
+    },
+  },
+}
